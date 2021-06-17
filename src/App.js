52b0c026f6c5fd1694e8f9c6a4c031982b2bb23d
booldook <booldook@gmail.com>
2021-06-17 15:04:45 +0900

    styledComponents

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import React, { useState, useCallback, useEffect, useMemo } from 'react'
+import Container from 'react-bootstrap/Container'
+import TitleBar from './components/TitleBar'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './scss/App.scss'
+
 const App = () => {
+	const title = {
+		mainTitle: 'React를 배워보자',
+		subTitle: 'react-create-app / styled-components / react-bootstrap'
+	}
+	const design = {
+		bgColor: '#232323',
+		color: '#f9f9f9'
+	}
 	return (
-		<div>
-			<h1>Hello, React</h1>
-		</div>
+		<Container>
+			<TitleBar title={ title } design={ design } />
+		</Container>
 	)
 }
 
--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React from 'react'
+import styled, { css } from 'styled-components'
 
-const TitleBar = () => {
+const Wrapper = styled.div`
+	padding: 1.5em; border-radius: .5em; margin: 1em 0;
+	background-color: ${ props => props.bgColor ? props.bgColor : '#ddd' };
+	color: ${ props => props.color ? props.color : '#111' };
+	`
+const Maintitle = styled.h1`
+	font-size: 2em;
+`
+const Subtitle = styled.h2`
+	font-size: 1.25em;
+	color: #bbb;
+`
+
+const style = {
+	borderColor: "#666", 
+	borderWidth: "1px"
+}
+
+const TitleBar = ({ title, design }) => {
 	return (
-		<div>
-			
-		</div>
+		<Wrapper color={ design.color } bgColor={ design.bgColor }>
+			<Maintitle>{ title.mainTitle }</Maintitle>
+			<hr style={style} />
+			<Subtitle>{ title.subTitle }</Subtitle>
+		</Wrapper>
 	);
 }
 
