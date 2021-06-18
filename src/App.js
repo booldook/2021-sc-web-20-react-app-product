dc07b76f2866f23a94557a1da6850163225a9c1a
booldook <booldook@gmail.com>
2021-06-18 11:22:59 +0900

    App async 구현 전

--- src/App.js
+++ src/App.js
@@ -9,15 +9,32 @@ import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/App.scss'
 
+const title = {
+	mainTitle: 'React를 배워보자',
+	subTitle: 'react-create-app / styled-components / react-bootstrap'
+}
+const design = {
+	bgColor: '#232323',
+	color: '#f9f9f9'
+}
+
 const App = () => {
-	const title = {
-		mainTitle: 'React를 배워보자',
-		subTitle: 'react-create-app / styled-components / react-bootstrap'
-	}
-	const design = {
-		bgColor: '#232323',
-		color: '#f9f9f9'
-	}
+	const [evt, setEvt] = useState([])
+	const [searchEvt, setSearchEvt] = useState([])
+	const [err, setErr] = useState(null)
+
+	useEffect(async () => {
+		try {
+
+		}
+		catch(err) {
+			setErr(err)
+		}
+		finally {
+			return () => {}
+		}
+	}, [])
+
 	const handleChange = v => {
 		// Search
 	}
